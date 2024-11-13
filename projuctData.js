f2756a2bfbc55c56292721b796cadf74889055a6
chogueur amine <chogueuramine@gmail.com>
2024-11-13 13:17:04 -0800

    update my projects section

--- projuctData.js
+++ projuctData.js
@@ -15,8 +15,49 @@ export const myProjects = [
     linkToWeb: ["https://made4cloud.vercel.app/", "https://made4cloud.com"],
     linkToCode: "https://github.com/Amin-Chogueur/made4cloud",
   },
+
   {
     id: 2,
+    title: "Book Library App",
+    description:
+      "A full-stack app built for a client’s book library, allowing users to browse, search by title, filter by category, and place orders. Enhances customer engagement and supports seamless online sales.",
+    technologies: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    ],
+    details:
+      "This full-stack Book Library App was developed for a client who runs a library specializing in book sales. The application enables users to browse an extensive collection of books, search by title, and filter by category to find exactly what they’re looking for. Users can easily place orders through the app, offering a streamlined and enjoyable shopping experience. The app is designed to facilitate the client’s business operations and enhance customer engagement, making it an essential tool for their online presence.",
+    image: "/ghiz1.png",
+    imagesDetails: ["/ghiz2.png", "/ghiz3.png"],
+    direction: "right",
+    button: "Details",
+    linkToWeb: ["https://ghiz-read.vercel.app/"],
+    linkToCode: "https://github.com/Amin-Chogueur/ghiz-read",
+  },
+  {
+    id: 3,
+    title: "Book Library Admin Panel",
+    description:
+      "A secure, client-only platform for managing book inventory. Allows easy updates, organization, and image uploads to keep the catalog current. Features robust authentication for exclusive access.",
+    technologies: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
+    ],
+    details:
+      "The Book Library Admin Panel is a secure, custom-built platform exclusively accessible to the client. It allows the client to easily manage their book inventory by adding, updating, and organizing book listings, including cover images and categories. Designed with an intuitive interface, the admin panel enables efficient catalog management, ensuring that the online store remains up-to-date with the latest titles available for customers. Robust authentication ensures only the client has access.",
+    image: "/admin.png",
+    imagesDetails: ["/admin1.png", "/admin2.png"],
+    direction: "left",
+    button: "Details",
+    linkToWeb: ["https://ghiz-read-admin.vercel.app/"],
+    linkToCode: "https://github.com/Amin-Chogueur/Book-App-Admin-panel",
+  },
+  {
+    id: 4,
     title: "website for soufflesnacks company",
     description:
       "A real-world project for SouffleSnake, a company specializing in producing chips and snacks. ",
@@ -32,45 +73,5 @@ export const myProjects = [
     button: "Details",
     linkToWeb: ["https://soufflesnacks.vercel.app/"],
     linkToCode: "https://github.com/Amin-Chogueur/soufflesnacks",
-  },
-  {
-    id: 3,
-    title: "Book Library App",
-    description:
-      "A full-stack app built for a client’s book library, allowing users to browse, search by title, filter by category, and place orders. Enhances customer engagement and supports seamless online sales.",
-    technologies: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
-    ],
-    details:
-      "This full-stack Book Library App was developed for a client who runs a library specializing in book sales. The application enables users to browse an extensive collection of books, search by title, and filter by category to find exactly what they’re looking for. Users can easily place orders through the app, offering a streamlined and enjoyable shopping experience. The app is designed to facilitate the client’s business operations and enhance customer engagement, making it an essential tool for their online presence.",
-    image: "/ghiz1.png",
-    imagesDetails: ["/ghiz2.png", "/ghiz3.png"],
-    direction: "left",
-    button: "Details",
-    linkToWeb: ["https://ghiz-read.vercel.app/"],
-    linkToCode: "https://github.com/Amin-Chogueur/ghiz-read",
-  },
-  {
-    id: 4,
-    title: "Book Library Admin Panel",
-    description:
-      "A secure, client-only platform for managing book inventory. Allows easy updates, organization, and image uploads to keep the catalog current. Features robust authentication for exclusive access.",
-    technologies: [
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg",
-      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
-    ],
-    details:
-      "The Book Library Admin Panel is a secure, custom-built platform exclusively accessible to the client. It allows the client to easily manage their book inventory by adding, updating, and organizing book listings, including cover images and categories. Designed with an intuitive interface, the admin panel enables efficient catalog management, ensuring that the online store remains up-to-date with the latest titles available for customers. Robust authentication ensures only the client has access.",
-    image: "/admin.png",
-    imagesDetails: ["/admin1.png", "/admin2.png"],
-    direction: "right",
-    button: "Details",
-    linkToWeb: ["https://ghiz-read-admin.vercel.app/"],
-    linkToCode: "https://github.com/Amin-Chogueur/Book-App-Admin-panel",
   },
 ];
